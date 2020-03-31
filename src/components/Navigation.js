2512c0a7cdcc7341fd44f27c289758ab6d503541
Hayoun Lee <sidhlee@gmail.com>
2020-03-31 20:37:46 +0900

    Add styles for Navigation and implement side nav toggle

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { ReactComponent as MenuIcon } from "../images/hamburger.svg";
+import { ReactComponent as CloseIcon } from "../images/closeicon.svg";
 
 export default function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleTogglerClick() {
+    setIsOpen(s => !s);
+  }
   return (
     <nav>
-      <ul>
+      <button
+        className={isOpen ? "nav-toggler is-open" : "nav-toggler"}
+        onClick={handleTogglerClick}
+      >
+        <MenuIcon className="menu-icon" />
+        <CloseIcon className="close-icon" />
+      </button>
+      <ul className={isOpen ? "is-open" : null}>
         <li>
           <a href="#home">home</a>
         </li>
