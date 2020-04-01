feb253308be2e82f1775f1ae2e430d23364df063
Hayoun Lee <sidhlee@gmail.com>
2020-04-01 09:56:40 +0900

    Add PortfolioModal + Fade Animation

--- src/App.js
+++ src/App.js
@@ -10,18 +10,33 @@ import Backdrop from "./components/Backdrop";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   function handleTogglerClick() {
     setIsOpen(s => !s);
+  }
+  function closeSideNav() {
+    setIsOpen(false);
+  }
+  function openModal() {
+    setIsModalOpen(true);
+  }
+  function closeModal() {
+    setIsModalOpen(false);
   }
 
   return (
     <div className="App">
       <Navigation clicked={handleTogglerClick} isOpen={isOpen} />
-      <Backdrop isOpen={isOpen} clicked={handleTogglerClick} />
+      <Backdrop isOpen={isOpen || isModalOpen} clicked={closeSideNav} />
       <Welcome />
       <Intro />
       <AboutMe />
-      <Portfolio />
+      <Portfolio
+        openModal={openModal}
+        isModalOpen={isModalOpen}
+        closeModal={closeModal}
+      />
       <Contact />
       <Footer />
     </div>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { lorem } from "faker";
 import { ReactComponent as CloseIcon } from "../images/closeicon.svg";
 
@@ -11,7 +11,7 @@ const requireImages = require.context(
 const images = requireImages.keys().map(path => requireImages(path));
 const imageObjects = images.map((src, i) => ({
   src,
-  id: i + 1,
+  id: i,
   alt: "portfolio item",
   featured: false,
   title: "Project Name",
@@ -21,13 +21,62 @@ const imageObjects = images.map((src, i) => ({
 
 imageObjects[0].featured = true;
 
-function PortfolioModal({ src, title, subtitle, alt }) {
+// Modal is always mounted with Portfolio (Only contents changes dynamically)
+function PortfolioModal({
+  src,
+  title,
+  subtitle,
+  alt,
+  closeModal,
+  isModalOpen
+}) {
+  const [fadeStarted, setFadeStarted] = useState(false);
+
+  const modalFadeOut = useCallback(() => {
+    setFadeStarted(true);
+  }, []);
+
+  useEffect(() => {
+    function handleKeydown(e) {
+      if (!isModalOpen) return; // don't run if modal is not opened
+      if (e.keyCode === 27) {
+        modalFadeOut();
+      }
+    }
+    window.addEventListener("keydown", handleKeydown);
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [modalFadeOut, isModalOpen]);
+
+  function handleAnimationEnd() {
+    if (fadeStarted) closeModal(); // Only close modal on fadeOut (don't close it when fadeIn animation ends)
+    setFadeStarted(false); // Reset fadeStarted to false so that modal won't get closed on revisit
+  }
+
+  const headerStyle = {
+    backgroundImage: `url(${src})` // no dq, no semicolon
+  };
+
+  const modalClass = isModalOpen
+    ? "portfolio-modal is-open"
+    : "portfolio-modal";
+
+  const modalStyle =
+    isModalOpen && fadeStarted
+      ? {
+          animation: "modalOut 300ms forwards"
+        }
+      : null;
+
   return (
-    <div className="portfolio-modal">
-      <button className="modal-close">
+    <div
+      className={modalClass}
+      style={modalStyle}
+      onAnimationEnd={handleAnimationEnd}
+    >
+      <button className="modal-close" onClick={modalFadeOut}>
         <CloseIcon />
       </button>
-      <div className="portfolio-header">
+      <div className="portfolio-header" style={headerStyle}>
         <div className="portfolio-title-box">
           <h2 className="portfolio-title">{title}</h2>
           <p className="portfolio-subtitle">{subtitle}</p>
@@ -42,12 +91,21 @@ function PortfolioModal({ src, title, subtitle, alt }) {
         <p>{lorem.words(30)}</p>
         <p>{lorem.words(30)}</p>
         <img className="portfolio-pull-right" src={src} alt={alt} />
+        <p>{lorem.words(30)}</p>
+        <p>{lorem.words(20)}</p>
       </div>
     </div>
   );
 }
 
-export default function Portfolio() {
+export default function Portfolio({ openModal, closeModal, isModalOpen }) {
+  const [modalIndex, setModalIndex] = useState(0);
+
+  function handleLinkClick(id) {
+    setModalIndex(id);
+    openModal();
+  }
+
   const portfolioItems = imageObjects.map(obj => (
     <figure
       key={obj.id}
@@ -57,20 +115,27 @@ export default function Portfolio() {
       <figcaption>
         <h2 className="portfolio-title">{obj.title}</h2>
         <p className="portfolio-desc">{obj.desc}</p>
-        <button className="portfolio-link">More info</button>
+        <button
+          onClick={() => handleLinkClick(obj.id)}
+          className="portfolio-link"
+        >
+          More info
+        </button>
       </figcaption>
-      <PortfolioModal
-        src={obj.src}
-        title={obj.title}
-        subtitle={obj.subtitle}
-        alt={obj.alt}
-      />
     </figure>
   ));
   return (
     <section className="portfolio" id="portfolio">
       <h1>My Work</h1>
       <div className="portfolio-items">{portfolioItems}</div>
+      <PortfolioModal
+        isModalOpen={isModalOpen}
+        closeModal={closeModal}
+        src={imageObjects[modalIndex].src}
+        title={imageObjects[modalIndex].title}
+        subtitle={imageObjects[modalIndex].subtitle}
+        alt={imageObjects[modalIndex].alt}
+      />
     </section>
   );
 }
